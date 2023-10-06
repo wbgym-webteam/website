ecc9bfb88ed92ae1d983e6a5f8e48b2bbb4c3932
micha003 <michael.vorogushyn@wbgym.de>
2023-10-06 14:37:35 +0200

    resolved #2

--- wbgym/wbgym/urls.py
+++ wbgym/wbgym/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("hauptseite.urls")),
+    path("", include("startpage.urls")),
 ]
