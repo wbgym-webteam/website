137035de3c70e9d623e0465ceb094d5a364a6d4b
micha003 <michael.vorogushyn@wbgym.de>
2023-10-25 13:47:56 +0200

    added basic routing

--- wbgym/main/views.py
+++ wbgym/main/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here. These are test views only for dev
+
+def home(request):
+    return render(request, 'main/home.html')
