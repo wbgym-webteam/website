6813cbf578234cc9fbe595bacb36400cca8ec479
micha003 <michael.vorogushyn@wbgym.de>
2023-10-25 14:07:03 +0200

    added simple article display

--- wbgym/main/views.py
+++ wbgym/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from .models import Article
 # Create your views here. These are test views only for dev
 
 def home(request):
-    return render(request, 'main/home.html')
+    # get all articles
+    articles = Article.objects.all()
+    
+    return render(request, 'main/home.html', {'articles': articles})
