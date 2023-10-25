b95f882ab011aec1e8a815a88f635e21aa583481
Milim <81323548+MilimTheTrueOne@users.noreply.github.com>
2023-10-25 13:47:27 +0200

    basíc models for db

--- wbgym/main/models.py
+++ wbgym/main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    year = models.PositiveSmallIntegerField()
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    # short hand like "herm" for "hermann" (last name)
+    short = models.CharField(max_length=10)
+
+class Article(models.Model):
+    text = models.TextField()
+
+
