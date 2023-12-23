b1251a6259c3fab2a73c1ad6227ae1c2d9167466
micha003 <michael.vorogushyn@wbgym.de>
2023-12-23 20:46:23 +0100

    [Frontend] added tailwindcss

--- wbgym/content/views.py
+++ wbgym/content/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .models import Article
+
 # Create your views here. These are test views only for dev
+
 
 def home(request):
     # get all articles
     articles = Article.objects.all()
 
-    return render(request, 'content/home.html', {'articles': articles})
+    return render(request, "content/home.html", {"articles": articles})
