1e5e161a3e63ddbc7d13db83d9a2812b4e1a9ad6
micha003 <michael.vorogushyn@wbgym.de>
2023-12-29 15:04:35 +0100

    [Doc] added README for Frontend Dev + updated requierements.py + added color schema for wbgym
    The new color schema is now available for TailwindCSS classes

--- wbgym/theme/README.md
+++ wbgym/theme/README.md
@@ -1,21 +1,60 @@
-# Guide for Developing Frontend
+# Guide for Frontend Development
+
 This guide will help you develop themes for the WBGym website. \
 **Contents**:
-- [What is used here?](#What-is-used-here?)
-- [Testing](#Testing)
 
-## What is used here?
-This website uses plain HTML with Jinja (a templating engine from Python Django). In addition we integrated TailwindCSS for styling, because it looks much better and is easier to code. There is no JS-Framework used, because it would be overkill this project. And we don't need that many interactive elements.
+- [Getting Started](#getting-started)
+- [Troubleshooting](#troubleshooting)
+- [Useful Links](#useful-links)
 
-## Testing
-To test the website, you need to rebuild the CSS with the following command:
+## Getting Started
+
+When you first clone the repository, you will need to install the dependencies. These are:
+
+- [Node.js](https://nodejs.org/en/)
+- Python 3.8 or higher
+- Python packages: `pip install -r requirements.txt`
+
+Once you have installed these, you can run the following command to install the Node.js dependencies:
+
+```bash
+python manage.py tailwind start
+```
+
+This will install the dependencies and if you run the command again, it will make a **development** build of the theme.
+
+To make a **production** build, run the following command:
 
 ```bash
 python manage.py tailwind build
 ```
 
-After that you can run the server with:
+Afterwards you can run the server with the following command:
 
 ```bash
 python manage.py runserver
 ```
+
+## Troubleshooting
+
+Oftentimes when you try to run the command `python manage.py tailwind start`, you will get an error which says, that you haven't installed npm (NodePackageManager).To fix this issue, follow the steps below:
+
+1. **Check your environment variables.** To do this, press the Windows key and search for "environment variables". Click on the button that says "Environment Variables" and then click on "Path" in the "System variables" section. Click on "Edit" and then click on "New". Paste the following path into the text box: `C:\Users\YOUR_USERNAME\AppData\Roaming\npm.cmd`. Replace `YOUR_USERNAME` with your username. Click on "OK" and then click on "OK" again. Now you should be able to run the command `python manage.py tailwind start` without any issues.
+
+2. **If the above step didn't work, try the following:**
+Restart your computer. Then run the command `python manage.py tailwind start` again. If it still doesn't work, continue to the next step.
+
+3. **Check the `NPM_BIN_PATH` variable** in the `wbgym/settings.py`-File. If it is the same path as the actual path to the npm.cmd on your computer (test it by inserting the path into the file explorer) and it's still not working, open an issue on Github. Do it the following way:
+
+- Title: "NPM_BIN_PATH is wrong"
+- Labels: bug, help wanted, Django, duplicate (if there is already an issue about this)
+- Description: write, which steps you've already tried to fix the issue and what the output of the command `python manage.py tailwind start` is. Also write, what you've done beyond the steps in this guide.
+
+## Useful Links
+
+- [Setup and run Tailwind with Python Django](https://django-tailwind.readthedocs.io/en/latest/installation.html)
+- [Tailwind CSS](https://tailwindcss.com/)
+- [Tailwind CSS Documentation](https://tailwindcss.com/docs)
+- [Tailwind CSS Cheat Sheet](https://nerdcave.com/tailwind-cheat-sheet)
+- [Tailwind CSS Color Palette](https://tailwindcss.com/docs/customizing-colors#color-palette-reference)
+- [Tailwind CSS Color Palette Generator](https://javisperez.github.io/tailwindcolorshades/)
--- wbgym/theme/static_src/tailwind.config.js
+++ wbgym/theme/static_src/tailwind.config.js
@@ -6,52 +6,68 @@
  */
 
 module.exports = {
-    content: [
-        /**
-         * HTML. Paths to Django template files that will contain Tailwind CSS classes.
-         */
+  content: [
+    /**
+     * HTML. Paths to Django template files that will contain Tailwind CSS classes.
+     */
 
-        /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
-        '../templates/**/*.html',
+    /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
+    "../templates/**/*.html",
 
-        /*
-         * Main templates directory of the project (BASE_DIR/templates).
-         * Adjust the following line to match your project structure.
-         */
-        '../../templates/**/*.html',
+    /*
+     * Main templates directory of the project (BASE_DIR/templates).
+     * Adjust the following line to match your project structure.
+     */
+    "../../templates/**/*.html",
 
-        /*
-         * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
-         * Adjust the following line to match your project structure.
-         */
-        '../../**/templates/**/*.html',
+    /*
+     * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
+     * Adjust the following line to match your project structure.
+     */
+    "../../**/templates/**/*.html",
 
-        /**
-         * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
-         * patterns match your project structure.
-         */
-        /* JS 1: Ignore any JavaScript in node_modules folder. */
-        // '!../../**/node_modules',
-        /* JS 2: Process all JavaScript files in the project. */
-        // '../../**/*.js',
+    /**
+     * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
+     * patterns match your project structure.
+     */
+    /* JS 1: Ignore any JavaScript in node_modules folder. */
+    // '!../../**/node_modules',
+    /* JS 2: Process all JavaScript files in the project. */
+    // '../../**/*.js',
 
-        /**
-         * Python: If you use Tailwind CSS classes in Python, uncomment the following line
-         * and make sure the pattern below matches your project structure.
-         */
-        // '../../**/*.py'
-    ],
-    theme: {
-        extend: {},
+    /**
+     * Python: If you use Tailwind CSS classes in Python, uncomment the following line
+     * and make sure the pattern below matches your project structure.
+     */
+    // '../../**/*.py'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        weinberg: {
+          50: "#F7EDF7",
+          100: "#EDDAEC",
+          200: "#D6A9D3",
+          300: "#BD7DB8",
+          400: "#8A3883",
+          500: "#560D4F",
+          600: "#4F0B45",
+          700: "#400733",
+          800: "#330526",
+          900: "#26031A",
+          950: "#1A010F",
+        },
+      },
     },
-    plugins: [
-        /**
-         * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
-         * for forms. If you don't like it or have own styling for forms,
-         * comment the line below to disable '@tailwindcss/forms'.
-         */
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/aspect-ratio'),
-    ],
-}
+  },
+  plugins: [
+    /**
+     * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
+     * for forms. If you don't like it or have own styling for forms,
+     * comment the line below to disable '@tailwindcss/forms'.
+     */
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
