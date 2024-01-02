eed95fd878bda7bfc73ead22f71f001e34eca948
micha003 <michael.vorogushyn@wbgym.de>
2024-01-02 21:27:58 +0100

    added first basic view to new app

--- wbgym/MeinBereich/views.py
+++ wbgym/MeinBereich/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def dashboard(request):
+    return render(request, "MeinBereich/dashboard.html")
--- wbgym/wbgym/urls.py
+++ wbgym/wbgym/urls.py
@@ -20,6 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("content.urls")),
-    path("", include("MeinBereich.urls")),
+    path("MeinBereich/", include("MeinBereich.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
