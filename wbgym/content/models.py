9dadd252c6fd1da0ef038609cd73751d702a7238
micha003 <michael.vorogushyn@wbgym.de>
2024-02-04 09:33:23 +0100

    [Login] Redirect is working from Login Page to personal dashboard

--- wbgym/content/models.py
+++ wbgym/content/models.py
@@ -9,6 +9,7 @@ class Student(models.Model):
     email = models.EmailField()
     year = models.PositiveSmallIntegerField()
 
+
 class Teacher(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -16,5 +17,7 @@ class Teacher(models.Model):
     # short hand like "herm" for "hermann" (last name)
     short = models.CharField(max_length=10)
 
+
 class Article(models.Model):
+    # TODO: Make it more complex, add aditional fields; maybe store even html files?
     text = models.TextField()
--- wbgym/content/views.py
+++ wbgym/content/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Article
+from django.http import HttpResponseRedirect
 
 # Create your views here. These are test views only for dev
 
@@ -8,15 +9,16 @@ def home(request):
     if request.method == "POST":
         email = request.POST["email"]
         password = request.POST["password"]
-        loginAsAdmin = request.POST["login_as_admin"]
+        # FIXME: loginAsAdmin is not working properly
+        # loginAsAdmin = request.POST["login_as_admin"]
 
-        if loginAsAdmin == "on":
-            loginAsAdmin = True
-        else:
-            loginAsAdmin = False
+        # if loginAsAdmin == "on":
+        #     loginAsAdmin = True
+        # else:
+        #     loginAsAdmin = False
 
-        print(f"{email} {password} {loginAsAdmin}")
-        return redirect("home")
+        print(f"{email} {password} ")  # TODO: add loginAsAdmin, when it is implemented
+        return HttpResponseRedirect("/MeinBereich/dashboard")
     else:
         pass
     # get all articles
